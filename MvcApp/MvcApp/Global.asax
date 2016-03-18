7a16ecde0a65929fd521d667386dc2056a01af12
sachinnx <supernova780@gmail.com>
2016-03-18 17:21:23 +0530

    opened MVC app

---
+++ MvcApp/MvcApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="MvcApp.MvcApplication" Language="VB" %>
---
+++ MvcApp/MvcApp/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("MvcApp")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Capgemini")> 
+<Assembly: AssemblyProduct("MvcApp")> 
+<Assembly: AssemblyCopyright("Copyright © Capgemini 2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+' The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("1c0dd849-0c36-4c5d-9644-ea46de5c2b44")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
